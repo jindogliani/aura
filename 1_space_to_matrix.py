a1b044de35b2195cc0ac7fa011a933881dc049fd
jindogliani <jindogliani@kaist.ac.kr>
2023-05-14 17:07:04 +0900

    #2.py done

---
+++ 1_space_to_matrix.py
@@ -0,0 +1,7 @@
+#Unity 상에서 공간 캡처 데이터를 불러와서 매트릭스로 변형 및 .csv로 저장
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+import json
---
+++ practice.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+print("============================ Practice Starts! ============================")
+
+array1 = np.arange(4)
+# numps
+print(array1)
+
+array2 = np.zeros((4,4), dtype = float)
+# 
+print(array2)
+
+array3 = np.ones((4,4), dtype = str)
+
+print(array3)
+
+
