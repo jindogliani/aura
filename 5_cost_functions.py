0288ed5d9955429dca4b7d00cbdb2809b5a0faa2
jindogliani <jindogliani@kaist.ac.kr>
2023-09-16 15:41:01 +0900

    cost functions

--- 5_cost_functions.py
+++ 5_cost_functions.py
@@ -12,35 +12,32 @@ import seaborn as sns
 import random
 import math
 
-with open('wall_list_2022.pkl', 'rb') as f:
-    wall_list = pickle.load(f)
+# goal cost function 에 들어감
+optimized_artwork_heatmap = np.load('optimized_artwork_heatmap.npy')
 
+# regularization function 에 들어감
 with open('optimized_wall_list_sample.pkl', 'rb') as f:
     optimized_wall_list = pickle.load(f)
 
+# artwork similarity function 에 들어감
 with open('optimized_artwork_list_sample.pkl', 'rb') as f:
     optimized_artwork_list = pickle.load(f)
 
-sample_scene_dict = dict()
-
-optimized_artwork_heatmap = np.load('optimized_artwork_heatmap.npy')
-
+# 작가 정보를 아트워크 리스트에 안 넣어서 일단 넣음
 for artwork in optimized_artwork_list:
     artwork['artist'] = 'not_set'
-    sample_scene_dict[artwork['id']] = (artwork['wall'], random.randint(2, 50))
     print(artwork)
-
 for wall in optimized_wall_list:
     print(wall)
 
-heatmapCSV = pd.DataFrame(optimized_artwork_heatmap)
-sns.heatmap(heatmapCSV, cmap='RdYlGn_r', vmin=-20, vmax=100)
-plt.show()
+init_cell_variance = 2000
+init_regulation_variance = 2000
+init_WCSS = 2000
 
 def goal_cost(optimized_artwork_heatmap):
     # 음수는 값에 안 넣게 필터 필요
     variance = np.var(optimized_artwork_heatmap, ddof = 1)
-    return variance #노멀라이즈 이전
+    return variance
 
 def regularization_cost(optimized_wall_list):
     new_artwork_positions = []
@@ -48,16 +45,23 @@ def regularization_cost(optimized_wall_list):
     for wall in optimized_wall_list:
         new_artwork_positions.append(wall['hanged_artworks']['new_coords']) 
     for i in range(len(new_artwork_positions)):
-        if i < len(new_artwork_positions) - 1:
-            new_artwork_distance.append(math.dist(new_artwork_positions[i], new_artwork_positions[i+1])) 
-        else: new_artwork_distance.append(math.dist(new_artwork_positions[i], new_artwork_positions[0])) 
+        j = (i+1) % len(new_artwork_positions)
+        new_artwork_distance.append(math.dist(new_artwork_positions[i], new_artwork_positions[j]))
     new_artwork_distance_arr = np.array(new_artwork_distance)
     variance = np.var(new_artwork_distance_arr, ddof = 1)
     return variance #노멀라이즈 이전
 
+def centroid(coordinates):
+    x_coords = [p[0] for p in coordinates]
+    y_coords = [p[1] for p in coordinates]
+    _len = len(coordinates)
+    centroid_x = sum(x_coords)/_len
+    centroid_y = sum(y_coords)/_len
+    return (centroid_x, centroid_y)
+
 def similarity_cost(optimized_artwork_list):
     artist_list = ["한놈", "두식이", "석삼", "너구리", "오징어", "육개장"] #작가 수가 10 미만이라 그냥 리스트 만들고 진행, 나중에 돌리기 전에 지정
-    artist_variance_list = []
+    cluster_variance_list = []
     for artist in artist_list:
         same_artist_coords_list = []
         same_artist_dist_list = []
@@ -66,16 +70,20 @@ def similarity_cost(optimized_artwork_list):
             if artwork['artist'] == artist:
                 same_artist_coords_list.append(artwork['new_coords'])
 
-        for i in range(len(same_artist_coords_list)):
-            if i < len(same_artist_coords_list) - 1:
-                same_artist_dist_list.append(math.dist(same_artist_coords_list[i], same_artist_coords_list[i+1])) 
-            else: same_artist_dist_list.append(math.dist(same_artist_coords_list[i], same_artist_coords_list[0])) 
-        
+        centroid_coords = centroid(same_artist_coords_list)
+
+        for coords in same_artist_coords_list:
+            same_artist_dist_list.append(math.dist(coords, centroid_coords))
+
         same_artist_dist_arr = np.array(same_artist_dist_list)
         same_artist_dist_var = np.var(same_artist_dist_arr, ddof = 1)
-        artist_variance_list.append(same_artist_dist_var)
+        cluster_variance_list.append(same_artist_dist_var)
 
-    variance_sum = sum(artist_variance_list)
-    return variance_sum #노멀라이즈 이전
+    WCSS = sum(cluster_variance_list)
 
+    return WCSS
 
+WCSS = similarity_cost(optimized_artwork_list)
+norm_WCSS = WCSS / init_WCSS
+if (norm_WCSS >= 1):
+    norm_WCSS = 1
