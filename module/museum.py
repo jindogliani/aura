e02f261d62f4b2f067a5973c8e5e1de606232d5b
jindogliani <jindogliani@kaist.ac.kr>
2023-09-24 15:32:09 +0900

    micro change

--- module/museum.py
+++ module/museum.py
@@ -77,7 +77,7 @@ class MuseumScene():
     def update_scene(self, scene_data):
         for k, v in scene_data.items():
             if type(v[1]) == float or v[1] < 0 or v[1] > int(self.wall_data[v[0]]['length']*10):
-                raise "fuck your self"
+                raise "fuck yourself"
         self.scene_data = scene_data
         for wall_id in self.wall_data.keys():
             self.art_in_wall[wall_id] = sorted([art for art, (wall, pos) in self.scene_data.items() if wall == wall_id], key=lambda x: self.scene_data[x][1])
@@ -185,7 +185,7 @@ class MuseumScene():
             new_scene[art] = (_wall, _pos+value)
             for k, v in new_scene.items():
                 if type(v[1]) == float or v[1] < 0 or v[1] > int(self.wall_data[v[0]]['length']*10):
-                    raise "fuck your self"
+                    raise "fuck yourself"
             return new_scene
         
         
@@ -208,8 +208,7 @@ class MuseumScene():
 
     def visualize(self, num):
         visualization(best_scene_data, self.artwork_data, self.wall_data, num)
-        
-                    
+             
     # def print_scene(self):
     #     self.draw = {}
     #     for k, v in self.wall_data.items():
@@ -257,7 +256,7 @@ if __name__ == "__main__":
     
     #total_cost = scene.evaluation()
     #print(total_cost)
-    idx = 900
+    idx = 700
     scene.visualize(idx)
     
 
