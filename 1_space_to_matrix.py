3845716b534e12ed2d1b05162398f813223fcca7
jindogliani <jindogliani@kaist.ac.kr>
2023-09-27 13:54:27 +0900

    file arrangement

---
+++ 1_space_to_matrix.py
@@ -0,0 +1,102 @@
+"""
+2023/08/05
+- 캡처 이미지 데이터를 공간 데이터로 활용하는데 어려움이 있음.
+- 따라서 좌표 (coords) 데이터를 불러오는 방식으로 변경함.
+- 2022년 하정웅미술관, 2023년 광주시립미술관 전체 데이터 사용
+"""
+
+import cv2
+import numpy as np
+import math
+import csv
+import copy
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from time import localtime, time
+import os
+import pickle
+
+date = '+' + '(' + str(localtime(time()).tm_mon) +'-'+ str(localtime(time()).tm_mday) +'-'+ str(localtime(time()).tm_hour) + '-'+ str(localtime(time()).tm_min) + ')'
+
+def space_csv_to_heatmap(ver, spaceDataCSV, heatmap_cell_size, space_vertical_size, space_horizontal_size, x_offset, z_offset):
+    
+    spaceDataCSVname = ver + spaceDataCSV.name[10:-4]
+    
+    reader = csv.reader(spaceDataCSV)
+    #walls=edges와 같은 말. 벽의 값을 갖고 오기 위해 좌표배열1, 2를 합침
+    pointsArr= np.array(list(reader)) 
+    points2Arr= copy.deepcopy(pointsArr) 
+    points2Arr = np.roll(pointsArr, -1, axis=0)
+    edgeArr = np.concatenate((pointsArr, points2Arr), axis=1)
+    spaceDataCSV.close()
+
+    spaceVertcalCells, spaceHorizontalCells = space_vertical_size / heatmap_cell_size, space_horizontal_size / heatmap_cell_size
+    spaceHorizontalCells, spaceVertcalCells = round(spaceHorizontalCells), round(spaceVertcalCells)
+    heatmap = np.zeros((spaceVertcalCells, spaceHorizontalCells), dtype = np.uint8)
+    
+    img = np.zeros((spaceVertcalCells, spaceHorizontalCells), dtype = np.uint8)
+    li = []
+
+    for row in edgeArr:
+        x1, x2, z1, z2 = -float(row[0]), -float(row[3]), float(row[2]), float(row[5])
+        _x1, _x2, _z1, _z2= int((x1+x_offset)/heatmap_cell_size), int((x2+x_offset)/heatmap_cell_size), int((z1+z_offset)/heatmap_cell_size), int((z2+z_offset)/heatmap_cell_size) #z축 뒤집히는 것은 유동적 수정 필요
+        img = cv2.line(img, (_x1, _z1), (_x2, _z2), 255, 1)
+        wallDic = dict()
+        wallDic = {'id': '', 'displayable': True, 'length':0, 'theta':0, 'x1': 0, 'z1': 0, 'x2': 0,'z2': 0}
+        wallDic['length']= math.dist((x1, z1), (x2, z2))
+        wallDic['theta'] = abs(round(np.rad2deg(np.arctan2(z1 - z2, x1 - x2))) - 180) #TODO 2023
+        
+        if abs(wallDic['theta'] - 360) <= 5:
+            wallDic['theta'] = 0
+        
+        wallDic['x1'], wallDic['x2'], wallDic['z1'], wallDic['z2'] = x1, x2, z1, z2
+        li.append(wallDic)
+
+    for i in range(len(li)):
+        li[i]['id'] = 'w'+str(i)
+        if li[i]['length'] <= 1.3:
+            li[i]['displayable'] = False
+        else:
+            li[i]['displayable'] = True
+        print(li[i])
+
+    with open('wall_list_' + ver + '.pkl', 'wb') as f:
+        pickle.dump(li,f)
+
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    inside_img = cv2.drawContours(img.copy(), contours, -1, 127, thickness = -1)
+
+    resized_img = cv2.resize(img, (int(spaceHorizontalCells), int(spaceVertcalCells))) #1px == 10cm
+    resized_inside_img = cv2.resize(inside_img, (int(spaceHorizontalCells), int(spaceVertcalCells)))
+
+    _, resized_img_th = cv2.threshold(resized_img, 127, 255, cv2.THRESH_BINARY)
+
+    _resized_img_th = np.asarray(resized_img_th, dtype = np.int16)
+    _resized_inside_img = np.asarray(resized_inside_img, dtype = np.int16)
+    _resized_img_th = _resized_img_th + _resized_inside_img
+
+    sMapCSV = pd.DataFrame(_resized_img_th)
+    sns.heatmap(sMapCSV, cmap='Greens', vmin=0, vmax=255)
+    plt.show()
+
+    #np.save("SpaceData/" + spaceDataCSVname + date, _resized_img_th)
+    sMapCSV.to_csv("SpaceData/" + spaceDataCSVname + date + '_Heatmap.csv', index=False)
+
+
+ver = "2023"
+
+if ver == "2023":
+    spaceDataCSV = open('SpaceData/coords_GMA2.csv', 'r', encoding='utf-8-sig')
+    space_vertical_size, space_horizontal_size = 40, 40
+    heatmap_cell_size = 0.1
+    x_offset, z_offset = 7, 12 
+
+    space_csv_to_heatmap(ver, spaceDataCSV, heatmap_cell_size, space_vertical_size, space_horizontal_size)
+elif ver == "2022":
+    spaceDataCSV = open('SpaceData/coords_GMA2.csv', 'r', encoding='utf-8-sig')
+    space_vertical_size, space_horizontal_size = 40, 40
+    heatmap_cell_size = 0.1
+    x_offset, z_offset = 7, 12 
+
+    space_csv_to_heatmap(ver, spaceDataCSV, heatmap_cell_size, space_vertical_size, space_horizontal_size)
---
+++ Not_In_Use/test.py
@@ -0,0 +1,48 @@
+import os
+from time import localtime, time
+import json
+import math
+import pickle
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# import os
+
+# cwd = os.getcwd()
+# artwork_data_path = "Daegu_new.json"
+# visitor_data_path = "VisitorData/preAURA_1025_1030.csv"
+# artwork_data_filename, _ = os.path.splitext(os.path.basename(artwork_data_path))
+# visitor_data_filename, _ = os.path.splitext(os.path.basename(visitor_data_path))
+
+# print(visitor_data_filename)
+
+
+dict = {'A1':['w4', 10], 'A2':['w1', 20], 'A6':['w1', 10], 'A3':['w6', 30], 'A4':['w2', 40], 'A7':['w4', 1], 'A5':['w5', 50], 'A8':['w2', 1]}
+keys = list(dict.keys())
+values = list(dict.values())
+z = {k:v for v, k in sorted(zip(values, keys), key=(lambda x : (x[0][0], x[0][1])))}
+print(z)
+
+init_scene_data = {'PA-0023': ['w5', 28], 'PA-0026': ['w5', 65], 'KO-0009': ['w6', 29], 'PA-0095': ['w8', 12], 'PA-0098': ['w8', 33], 'PA-0076': ['w8', 49], 'PA-0074': ['w9', 16], 'PA-0075': ['w9', 31], 'PA-0077': ['w9', 49], 'KO-0010': ['w9', 80], 'KO-0008': ('w9', 116), 'PA-0101': ['w9', 132], 'PA-0057': ['w10', 11], 'PA-0052': ['w10', 29], 'PA-0061': ['w10', 49], 'PA-0001': ['w14', 33], 'PA-0003': ['w18', 41], 'PA-0004': ['w18', 93], 'PA-0082': ['w24', 17], 'PA-0084': ['w24', 48], 'PA-0083': ['w26', 21], 'PA-0063': ['w26', 46], 'PA-0067': ['w27', 22], 'PA-0064': ['w27', 49], 'PA-0024': ['w31', 53], 'PA-0087': ['w41', 28], 'PA-0027': ['w42', 16], 'PA-0025': ['w42', 46], 'PA-0036': ['w43', 19], 'PA-0085': ['w43', 40], 'PA-0086': ['w43', 55], 'PA-0070': ['w45', 12], 'PA-0065': ['w45', 50], 'PA-0031': ['w46', 35], 'PA-0088': ['w50', 34], 'PA-0100': ['w52', 29], 'PA-0099': ['w52', 58], 'KO-0007': ['w56', 33], 'KO-0006': ['w57', 49], 'KO-0004': ['w57', 86], 'KO-0005': ['w57', 112], 'PA-0090': ['w57', 154], 'PA-0089': ['w58', 34]}
+keys, values = list(init_scene_data.keys()), list(init_scene_data.values())
+for v in values:
+    print(v[0][1:])
+
+ordered_scene_data = {k:v for v, k in sorted(zip(values, keys), key=(lambda x : [int(x[0][0][1:]), x[0][1]]))}
+
+print(ordered_scene_data)
+
+
+visual_heatmap = np.load("initial_heatmap_2023_visualize.npy")
+initial_heatmap = np.load("initial_heatmap_2023.npy")
+
+_visual_heatmap = pd.DataFrame(visual_heatmap)
+_initial_heatmap = pd.DataFrame(initial_heatmap)
+
+sns.heatmap(_visual_heatmap, cmap='RdYlGn_r', vmin=-20, vmax=50)
+plt.show()
+sns.heatmap(_initial_heatmap, cmap='RdYlGn_r', vmin=-20, vmax=50)
+plt.show()
